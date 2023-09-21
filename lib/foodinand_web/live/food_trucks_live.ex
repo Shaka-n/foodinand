d6a1eacb01f86cbb9a4bcf186970ba18bfc9424e
Garen Sahagian <g.s.sahagian@gmail.com>
2023-09-21 14:27:24 -0400

    adding and tweaking styling

--- lib/foodinand_web/live/food_trucks_live.ex
+++ lib/foodinand_web/live/food_trucks_live.ex
@@ -5,20 +5,21 @@ defmodule FoodinandWeb.FoodTrucksLive do
 
   def render(assigns) do
     ~H"""
+    <div class="flex m-10 px-5 max-w-4xl">
     <div>
-      <div>
+    <h1 class=" font-bold text-xl"> Foodinand: The Food Explorer</h1>
+    <p>Discover new foods in SF! Search for a particular dish or cuisine in the bar below, or hit the random button for a random food truck!</p>
+      <div class="flex flex-col pt-5">
         <h2>Feeling Lucky? Hit the button for a random SF Food Truck!</h2>
-        <button phx-click="random-food-truck">Random</button>
+        <button class="rounded bg-emerald-600 px-3 py-1 hover:bg-gradient-to-br from-emerald-500 to-emerald-200 w-20 mt-2 text-white font-bold" phx-click="random-food-truck">Random</button>
         <div>
-          <%= if length(@random_food_trucks) > 0 do %>
             <%= for ft <- @random_food_trucks do %>
               <.food_truck_info ft={ft} />
             <% end %>
-          <% end %>
         </div>
       </div>
       <div>
-        <CoreComponents.simple_form for={@form} >
+        <CoreComponents.simple_form for={@form} class="w-3/4 max-w-xs">
         <CoreComponents.input
           type="text"
           placeholder="Type to Search by Food Item or Cuisine"
@@ -27,17 +28,14 @@ defmodule FoodinandWeb.FoodTrucksLive do
           phx-debounce="300"
         />
         </CoreComponents.simple_form>
-        <div>
+        <div >
             <%= for ft <- @food_truck_search_results do %>
-              <div>
-                <h3><%= ft.applicant %></h3>
-                <p><%= ft.address %></p>
-                <p> <%= ft.location_description %> </p>
-                <a href={ft.schedule}>Schedule</a>
-                <p> <%= ft.food_items %> </p>
-              </div>
+              <.food_truck_info ft={ft} />
             <% end %>
         </div>
+      </div>
+      </div>
+      <div>
       </div>
     </div>
     """
@@ -45,12 +43,15 @@ defmodule FoodinandWeb.FoodTrucksLive do
 
   def food_truck_info(assigns) do
     ~H"""
-      <div>
-        <h3><%= @ft.applicant %></h3>
-        <p><%= @ft.address %></p>
+      <div class="flex flex-col w-7/12 max-w-3/4 px-4 py-2 my-5 bg-gradient-to-b from-emerald-200 rounded">
+        <h3 class="font-semibold"><%= @ft.applicant %></h3>
+        <a class="text-sky-800" rel="noreferrer noopener" target="_blank" href={"https://www.google.com/maps/search/San+Francisco,+CA+#{@ft.address}"}><%= @ft.address %></a>
         <p> <%=@ft.location_description %> </p>
-        <a href="#"><%= @ft.schedule%></a>
-        <p> <%= @ft.food_items %> </p>
+        <a class="text-sky-800" href={@ft.schedule} rel="noreferrer noopener" target="_blank">Link to Schedule</a>
+        <div>
+          <h3> Menu </h3>
+          <p> <%= @ft.food_items %> </p>
+        </div>
       </div>
     """
   end
@@ -71,6 +72,6 @@ defmodule FoodinandWeb.FoodTrucksLive do
   def handle_event("random-food-truck", _, socket) do
     random_food_trucks = FoodTrucks.get_random_food_trucks()
     IO.inspect(random_food_trucks)
-    {:noreply, assign(socket, :random_food_truck, random_food_trucks)}
+    {:noreply, assign(socket, :random_food_trucks, random_food_trucks)}
   end
 end
