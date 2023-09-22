0d8b01cafa2949613e49b38c3a0c18f0d23e6617
Garen Sahagian <g.s.sahagian@gmail.com>
2023-09-22 10:01:43 -0400

    removing boilerplate phx test

--- test/foodinand_web/controllers/page_controller_test.exs
+++ test/foodinand_web/controllers/page_controller_test.exs
@@ -12,9 +12,4 @@ defmodule FoodinandWeb.PageControllerTest do
     conn = get(conn, ~p"/food-trucks")
     assert html_response(conn, 200) =~ "Foodinand: The Food Explorer"
   end
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
 end
